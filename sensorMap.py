7f728b856d9e27ec82140a318587b366e51aef98
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2016-08-16 16:54:15 +0800

    Modified the values of sensorMap to corrospond with current setting of raspberry pi and removed the extra key for the second sonar which was missed out in the previous removal.

--- sensorMap.py
+++ sensorMap.py
@@ -1,7 +1,7 @@
 def sensorMap():
-    station_1 = { 'motion_sensor' : 26 , 'sonar1' : [23,24] , 'sonar2' : [23,24] } #sonar : [trigger,echo]
+    station_1 = { 'motion_sensor' : 5 , 'sonar1' : [27,22] } #sonar : [trigger,echo]
             
-    station_2 = { 'motion_sensor' : 26 , 'sonar1' : [23,24] , 'sonar2' : [23,24] } 
+    station_2 = { 'motion_sensor' : 12 , 'sonar1' : [23,24] } 
                 
     sensorLocation = { 'station_1' : station_1 ,
                        'station_2' : station_2 } 
